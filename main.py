ef60fc472a218d5116e60f82141470da93ef2400
Igee Theron <igeetheron@gmail.com>
2025-08-26 18:39:08 +0200

    Can now provide file paths to the run comand

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words
 from stats import get_chars_dict
 from stats import chars_dict_to_sorted_list
@@ -8,8 +9,14 @@ def get_book_text(path_to_file):
     
 def main():
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    file_contents = get_book_text("books/frankenstein.txt")
+    
+    if (len(sys.argv) != 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path_to_file = sys.argv[1]
+    print(f"Analyzing book found at {path_to_file}...")
+    file_contents = get_book_text(path_to_file)
     num_words = get_num_words(file_contents)
     chars_dict = get_chars_dict(file_contents)
     chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
